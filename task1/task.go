89ef4268e59c308d672bc907d79212a3a12dfec9
Ника <mitichashviliveronika@gmail.com>
2024-06-24 04:27:38 +0300

    решение задачи 1

--- task1/task.go
+++ task1/task.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var a int
+	var b int
+	var sign string
+
+	fmt.Println("Введите числа: ")
+	fmt.Fscan(os.Stdin, &a)
+	fmt.Fscan(os.Stdin, &b)
+	fmt.Println("Введите операцию: ")
+	fmt.Fscan(os.Stdin, &sign)
+	if sign == "+" {
+		fmt.Println(a + b)
+	} else if sign == "-" {
+		fmt.Println(a - b)
+	} else if sign == "*" {
+		fmt.Println(a * b)
+	} else if sign == "/" {
+		fmt.Println(a / b)
+	}
+}
